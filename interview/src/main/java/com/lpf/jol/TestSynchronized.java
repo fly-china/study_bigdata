44707ce65eea2675268ed751ab063827fce59893
lenovo-win7 <463061655@qq.com>
2020-03-10 11:42:37 +0800

    测试synchronized修饰后的类的对象头变化

--- interview/src/main/java/com/lpf/jol/TestSynchronized.java
+++ interview/src/main/java/com/lpf/jol/TestSynchronized.java
@@ -7,14 +7,23 @@ import org.openjdk.jol.info.ClassLayout;
  * @create 2020-03-09 17:31
  **/
 public class TestSynchronized {
+
+    static Object lock = new Object();
+
     public static void main(String[] args) {
-        Object o = new Object();
-        synchronized (o) {
-            String layout = ClassLayout.parseInstance(o).toPrintable();
-            System.out.println(layout);
+
+        System.out.println("加锁前**********************");
+        String layout0 = ClassLayout.parseInstance(lock).toPrintable();
+        System.out.println(layout0);
+
+        System.out.println("***********加锁时***********");
+        synchronized (lock) {
+            String layout1 = ClassLayout.parseInstance(lock).toPrintable();
+            System.out.println(layout1);
         }
 
-        String layout = ClassLayout.parseInstance(o).toPrintable();
-        System.out.println(layout);
+        System.out.println("*********************释放锁后*");
+        String layout2 = ClassLayout.parseInstance(lock).toPrintable();
+        System.out.println(layout2);
     }
 }
