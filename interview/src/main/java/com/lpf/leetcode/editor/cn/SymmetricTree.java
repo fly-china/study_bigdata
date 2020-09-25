c6bf4c9304e1b85c5585feeb226b3c2417596b1c
lipengfei <463061655@qq.com>
2020-09-25 11:46:05 +0800

    [101]-对称二叉树的判断

--- interview/src/main/java/com/lpf/leetcode/editor/cn/SymmetricTree.java
+++ interview/src/main/java/com/lpf/leetcode/editor/cn/SymmetricTree.java
@@ -33,6 +33,8 @@
 
 package com.lpf.leetcode.editor.cn;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -42,25 +44,56 @@ public class SymmetricTree {
     public static void main(String[] args) {
         Solution solution = new SymmetricTree().new Solution();
 
+        Queue<TreeNode> queue = new LinkedList();
+        System.out.println(queue.poll());
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean isSymmetric(TreeNode root) {
-            // TODO：给定一个二叉树，检查它是否是镜像对称的。
-            if(root == null) return true;
-
-            // 左子树的栈（先左后右）
-            Stack<TreeNode> stackLeft = new Stack<>();
-            // 右子树的栈（先右后左）
-            Stack<TreeNode> stackRight = new Stack<>();
+            if (root == null) return true;
 
 
+            Queue<TreeNode> queue = new LinkedList();
+            queue.add(root.left);
+            queue.add(root.right);
 
-            return false;
+            while (!queue.isEmpty()) {
+                TreeNode left = queue.poll();
+                TreeNode right = queue.poll();
+
+                if (left == null && right == null) continue;
+                if (left == null || right == null) return false;
+                if (left.val != right.val) return false;
+
+                queue.add(left.right);
+                queue.add(right.left);
+                queue.add(left.left);
+                queue.add(right.right);
+            }
+
+            return true;
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
+
+    class Solution_RECUR {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) return true;
+            // 比较左右子树是否对称
+            return isSymmetric(root.left, root.right);
+        }
+
+        public boolean isSymmetric(TreeNode left, TreeNode right) {
+            if (left == null && right == null) return true;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+
+            // 左子树的左节点VS右子树的右节点  && 左子树的右节点VS右子树的左节点  都是相同的，才算对称
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+    }
 
 
     public class TreeNode {
