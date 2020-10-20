47cf446e87cce7ca215b9ee7d150a5c109127716
lipengfei <463061655@qq.com>
2020-10-20 19:42:43 +0800

    1、全排列-完成30%

--- interview/src/main/java/com/lpf/leetcode/editor/cn/Permutations.java
+++ interview/src/main/java/com/lpf/leetcode/editor/cn/Permutations.java
@@ -18,23 +18,46 @@
 
 package com.lpf.leetcode.editor.cn;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Permutations{
-  public static void main(String[] args) {
-	   Solution solution = new Permutations().new Solution();
-	   
-  }
-  
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        // TODO
+public class Permutations {
+    public static void main(String[] args) {
+        Solution solution = new Permutations().new Solution();
+        int[] nums = {1,2,3};
+        List<List<Integer>> lists = solution.permute(nums);
+        System.out.println("全排列数量：" + lists.size());
+        for (List<Integer> list : lists) {
+            list.forEach(System.out::print);
+            System.out.println("");
+        }
 
-        return null;
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            // TODO
+            List<List<Integer>> res = new ArrayList<>();
+            if (nums != null && nums.length > 0) {
+                backtrack(nums, 0, res, new ArrayList<Integer>());
+            }
+
+            return res;
+        }
+
+        private void backtrack(int[] nums, int index, List<List<Integer>> res, List<Integer> elemList) {
+            if (nums.length == index) {
+                res.add(elemList);
+                return;
+            }
+
+            elemList.add(nums[index]);
+            backtrack(nums, index + 1, res, elemList);
+
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-    
+
 }
